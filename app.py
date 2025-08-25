d51e36b9244502f04976935ee68e2948524f1a30
Mohit-Bharti-cell <mohit@netfotech.in>
2025-08-25 15:08:29 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ from flask import Flask
 from flask_cors import CORS
 from results.controller import results_bp
 
-def create_app(**kwargs):   # accept kwargs for gunicorn compatibility
+def create_app(**kwargs):   # <-- this fixes the gunicorn error
     app = Flask(__name__)
     CORS(app)
 
+    # register your blueprint
     app.register_blueprint(results_bp)
 
     @app.route("/")
@@ -13,6 +14,7 @@ def create_app(**kwargs):   # accept kwargs for gunicorn compatibility
         return "<h1>Backend OK</h1><p>Use <a href='/result'>/result</a></p>"
 
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
