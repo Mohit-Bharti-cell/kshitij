ea5ded6ff52d05b0b4ff8f76cd4d2573ee1913ae
Mohit-Bharti-cell <mohit@netfotech.in>
2025-08-28 14:48:46 +0530

    Delete results/controller.py

--- results/controller.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, jsonify
-from services.results_service import fetch_results
-
-results_bp = Blueprint("results", __name__)
-
-@results_bp.route("/result", methods=["GET"])
-def get_result():
-    data = fetch_results()
-    return jsonify(data)
