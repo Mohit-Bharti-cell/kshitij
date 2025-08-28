3bb1dc0fd529b70bf96d48d05e1b4cde38bc2543
Mohit-Bharti-cell <mohit@netfotech.in>
2025-08-28 17:38:37 +0530

    Delete services/results_service.py

--- services/results_service.py
+++
@@ -1,6 +0,0 @@
-from services.result_services_db import get_all_results, get_results_for_recruiter
-
-def fetch_results(recruiter_email: str | None = None):
-    if recruiter_email:
-        return get_results_for_recruiter(recruiter_email)
-    return get_all_results()
