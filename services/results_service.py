6813c38d800640e4e0f8c6ec21589ed9012c9156
Mohit-Bharti-cell <mohit@netfotech.in>
2025-09-09 18:17:23 +0530

    Delete services/results_service.py

--- services/results_service.py
+++
@@ -1,5 +0,0 @@
-from services.result_services_db import get_all_results
-
-def fetch_results(recruiter_id: str | None = None):
-    """Fetch results, optionally filtered by recruiter Mongo _id (as string)."""
-    return get_all_results(recruiter_id=recruiter_id)
